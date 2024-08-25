7f7e7f551f32a2ccfa3e02cb548702edc09f3636
nertc <davidtsiklauri7@gmail.com>
2024-08-25 14:17:26 +0400

    feat(subraces): add halfling subraces

--- script/subrace.js
+++ script/subrace.js
@@ -1,13 +1,41 @@
 const lightfoot = {
   id: "lightfoot",
   title: "Lightfoot",
-  info: [],
+  info: [
+    {
+      title: "Traits",
+      rows: [
+        {
+          title: "Charisma",
+          value: "+1",
+        },
+        {
+          title: "Naturally Stealthy",
+          value: "Can hide when obscured by at least 1 size larger creature",
+        },
+      ],
+    },
+  ],
 };
 
 const stout = {
   id: "stout",
-  title: "Sightfoot",
-  info: [],
+  title: "Stout",
+  info: [
+    {
+      title: "Traits",
+      rows: [
+        {
+          title: "Constitution",
+          value: "+1",
+        },
+        {
+          title: "Stout Resilience",
+          value: "Advantage on poison saving throws, poison damage is halved",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = {
